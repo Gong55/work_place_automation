113e04c7262fa0aad8952f18c546396540e520ff
Kong <gong2541@hotmail.co.th>
2024-09-23 01:06:53 +0700

    word.py

--- word.py
+++ word.py
@@ -1,17 +1,37 @@
+from docx import Document
+from docx.text.paragraph import Paragraph
+from docx.text.run import Run
+import os
 import pandas as pd
 import re
-from docx import Document
-import os
+from datetime import datetime
+import numpy as np
 
 
-excel_file = r'E:\Desktop\python\Word\data.xlsx' # Your File Location
-template_file = r'E:\Desktop\python\Word\base.docx'
-destination_folder = r'E:\Desktop\python\Word\output'
-
+excel_file = r'data_input.xlsx'
+template_file = r'SIC Draft Contract.docx'
+destination_folder = r'output'
+owner_contract = r'owner_contract.docx'
 
 df = pd.read_excel(excel_file)
-df = df.fillna('')
-os.makedirs(destination_folder, exist_ok=True)
+df = df.fillna(' ')
+df_flipped = df.set_index('Attributes').transpose()
+
+
+def convert_date_format(column):
+    df_flipped[column] = df_flipped[column].replace(' ', np.nan)
+    df_flipped[column] = pd.to_datetime(
+        df_flipped[column], errors='coerce')
+    df_flipped[f'{column}_day'] = df_flipped[column].dt.day
+    df_flipped[f'{column}_month'] = df_flipped[column].dt.month
+    df_flipped[f'{column}_year'] = df_flipped[column].dt.year
+
+
+convert_date_format("owner_passport_expire_date")
+convert_date_format("owner_passport_expire_date_2")
+
+print(df_flipped['owner_passport_expire_date_year'])
+print(df_flipped['owner_passport_expire_date_2_year'])
 
 
 def format_room(room):
@@ -23,23 +43,132 @@ def format_room(room):
     return room
 
 
-for index, row in df.iterrows():
-    new_doc = Document(template_file)
+def replace_text_with_format(paragraph, old_text, new_text):
+    for run in paragraph.runs:
+        if old_text in run.text:
+            original_font = run.font
+            original_text = run.text
+        if not pd.isna(new_text):
+            if old_text.isupper():
+                new_text = new_text.upper()
+            elif old_text.islower():
+                new_text = new_text.lower()
+            elif old_text.istitle():
+                new_text = new_text.title()
+        else:
+            new_text = ' '
 
-    for paragraph in new_doc.paragraphs:
-        if 'name' in paragraph.text:
-            paragraph.text = paragraph.text.replace('name', str(row['name']))
-        if 'date' in paragraph.text:
-            paragraph.text = paragraph.text.replace('date', str(row['date']))
-        if 'passport' in paragraph.text:
-            paragraph.text = paragraph.text.replace(
-                'passport', str(row['passport']))
-        if 'room' in paragraph.text:
-            paragraph.text = paragraph.text.replace('room', str(row['room']))
-            formatted_room = format_room(str(row['room']))
+        run.text = original_text.replace(old_text, str(new_text))
 
+        run.font.bold = original_font.bold
+        run.font.italic = original_font.italic
+        run.font.underline = original_font.underline
+        run.font.size = original_font.size
+        run.font.name = original_font.name
+        if original_font.color is not None:
+            run.font.color.rgb = original_font.color.rgb
+        run.font.all_caps = original_font.all_caps
+
+
+def replace_text_in_tables(table, old_text, new_text):
+    for row in table.rows:
+        for cell in row.cells:
+            for paragraph in cell.paragraphs:
+                replace_text_with_format(paragraph, old_text, new_text)
+
+
+for index, row in df_flipped.iterrows():
+    doc = Document(template_file)
+
+    for section in doc.sections:
+        footer = section.footer
+        for footer_text in footer.paragraphs:
+            replace_text_with_format(
+                footer_text, "-owner_name-", str(row['owner_name']))
+            replace_text_with_format(
+                footer_text, "owner_name_2", str(row['owner_name_2']))
+            replace_text_with_format(
+                footer_text, "tenant_name", str(row['tenant_name']))
+            replace_text_with_format(
+                footer_text, "tenant_name_2", str(row['tenant_name_2']))
+            replace_text_with_format(
+                footer_text, "witness_name", str(row['witness_name']))
+
+    for paragraph in doc.paragraphs:
+
+        replace_text_with_format(
+            paragraph, "project_name", str(paragraph['project_name']))
+        replace_text_with_format(
+            paragraph, "-room_number-", str(row['room_number']))
+        replace_text_with_format(
+            paragraph, "floor_number", str(row['floor_number']))
+        replace_text_with_format(paragraph, "area", str(row['area']))
+        replace_text_with_format(
+            paragraph, "building_no", str(row['building_no']))
+        replace_text_with_format(paragraph, "address", str(row['address']))
+        # address TH
+        # start_date
+        # end_date
+        # start_date TH
+        # end_date TH
+        replace_text_with_format(
+            paragraph, "rent_price", str(row['rent_price']))
+        # rent_price EN
+        # rent_price TH
+        # rent_price x2
+        # rent_price x2 EN
+        # rent_price x2 TH
+
+        replace_text_with_format(paragraph, "late_payment_grace_period", str(
+            row['late_payment_grace_period']))
+        #
+        replace_text_with_format(
+            paragraph, "-owner_name-", str(row['owner_name']))
+        replace_text_with_format(
+            paragraph, "owner_passport", str(['owner_passport']))
+        replace_text_with_format(
+            paragraph, "owner_nationality", str(['owner_nationality']))
+        replace_text_with_format(paragraph, "owner_bank", str(['owner_bank']))
+        replace_text_with_format(
+            paragraph, "owner_bank_branch", str('owner_bank_branch'))
+        replace_text_with_format(
+            paragraph, "owner_bank_account_no", str(['owner_bank_account_no']))
+        replace_text_with_format(
+            paragraph, "owner_bank_account_name", str(['owner_bank_account_name']))
+
+        # owner_passport_expire_date
+        # owner_passport_expire_date TH
+
+    formatted_room = format_room(str(row['room_number']))
     new_file_name = os.path.join(
-        destination_folder, f"output_{formatted_room}.docx")
-    new_doc.save(new_file_name)
+        destination_folder, f"Lease Agreement {row['project_name']} {formatted_room}.docx")
+    doc.save(new_file_name)
 
-print(f"'output_{formatted_room}.docx' created successfully")
+
+for index, row in df_flipped.iterrows():
+    doc2 = Document(owner_contract)
+    for table in doc2.tables:
+        replace_text_in_tables(table, "-NAME1-", str(row['owner_name']))
+        replace_text_in_tables(table, "-NANE2-", str(row['owner_name_2']))
+        replace_text_in_tables(table, "-PASPRT1-", str(row['owner_passport']))
+        replace_text_in_tables(
+            table, "-PASPRT1-", str(row['pwner_passport_2']))
+
+    for paragraph2 in doc2.paragraphs:
+        replace_text_with_format(
+            paragraph2, "-UNITNAME-", str(row['project_name']))
+        replace_text_with_format(
+            paragraph2, "-UNITNUMBER-", str(row['room_number']))
+        # replace_text_with_format(paragraph2, "-SY-", )
+        # replace_text_with_format(paragraph2, "-SM-", )
+        # replace_text_with_format(paragraph2, "-SD-", )
+        # replace_text_with_format(paragraph2, "-ENY-", )
+        # replace_text_with_format(paragraph2, "-ENM-", )
+        # replace_text_with_format(paragraph2, "-EMD-", )
+        # Have to convert excel date into DD MM YY
+
+    formatted_room = format_room(str(row['room_number']))
+    new_file_name = os.path.join(
+        destination_folder, f"代租管合約 {row['project_name']} {formatted_room}.docx")
+    doc2.save(new_file_name)
+    print(f"'output_{formatted_room}.docx' created successfully")
